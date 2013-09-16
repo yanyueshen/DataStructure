18d45cc0344788582ba14be2fc57498ed542c8e7
Yanyueshen <yanyueshen@163.com>
2013-09-16 09:58:19 +0800

    add directedGraphMatrixView

---
+++ DataStructure/algorithms/Graph/DirectedGraphMatrixView.h
@@ -0,0 +1,13 @@
+//
+//  DirectGraphMatrixView.h
+//  DataStructure
+//
+//  Created by Andrew on 13-9-16.
+//  Copyright (c) 2013年 Andrew. All rights reserved.
+//
+
+#import "GraphMatrixView.h"
+
+@interface DirectedGraphMatrixView : GraphMatrixView
+
+@end
---
+++ DataStructure/algorithms/Graph/DirectedGraphMatrixView.m
@@ -0,0 +1,31 @@
+//
+//  DirectGraphMatrixView.m
+//  DataStructure
+//
+//  Created by Andrew on 13-9-16.
+//  Copyright (c) 2013年 Andrew. All rights reserved.
+//
+
+#import "DirectedGraphMatrixView.h"
+
+@implementation DirectedGraphMatrixView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
